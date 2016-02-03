5fd1bcd42490d06e242d6bbc3c022c9311739313
Zhang Shuai <520zhangshuai@gmail.com>
2016-02-03 15:20:50 +0800

    Fixed findbugs.

---
+++ app/src/main/java/com/thoughtworks/startup/data/remote/SignInService.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.startup.data.remote;
+
+import com.thoughtworks.startup.data.model.SignInResponse;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+import rx.Observable;
+
+public interface SignInService {
+
+    String ENDPOINT = "http://10.29.2.74:3000"; // 106.187.43.111
+
+    int SUCCESS_CODE = 0;
+
+    @FormUrlEncoded
+    @POST("/login")
+    Observable<SignInResponse> signIn(@Field("email") String email, @Field("password") String password);
+
+    class Creator {
+
+        public static SignInService newSignInService() {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(ENDPOINT)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .build();
+            return retrofit.create(SignInService.class);
+        }
+    }
+}
