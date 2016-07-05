aef6261a232b848bb49906da896c50d66d69b0e5
Zhang Shuai <520zhangshuai@gmail.com>
2016-07-05 12:49:08 +0800

    Clean up response.

--- app/src/main/java/com/thoughtworks/startup/data/model/GitHubUserList.java
+++ app/src/main/java/com/thoughtworks/startup/data/model/GitHubUserList.java
@@ -9,51 +9,9 @@ import java.util.List;
 
 public class GitHubUserList {
 
-    @SerializedName("total_count")
-    @Expose
-    private Integer totalCount;
-    @SerializedName("incomplete_results")
-    @Expose
-    private Boolean incompleteResults;
     @SerializedName("items")
     @Expose
     private List<GitHubUser> items = new ArrayList<>();
-
-    /**
-     *
-     * @return
-     *     The totalCount
-     */
-    public Integer getTotalCount() {
-        return totalCount;
-    }
-
-    /**
-     *
-     * @param totalCount
-     *     The total_count
-     */
-    public void setTotalCount(Integer totalCount) {
-        this.totalCount = totalCount;
-    }
-
-    /**
-     *
-     * @return
-     *     The incompleteResults
-     */
-    public Boolean getIncompleteResults() {
-        return incompleteResults;
-    }
-
-    /**
-     *
-     * @param incompleteResults
-     *     The incomplete_results
-     */
-    public void setIncompleteResults(Boolean incompleteResults) {
-        this.incompleteResults = incompleteResults;
-    }
 
     /**
      *
